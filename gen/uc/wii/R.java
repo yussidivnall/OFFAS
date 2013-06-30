b8292ba48f35f01d7052ff6fd9e66c414ae5e1a1
uri <UC.dev.null@gmail.com>
2013-06-30 15:56:34 +0100

    all things (I guess i should remove the classes at some point, useless clutter)

--- gen/uc/wii/R.java
+++ gen/uc/wii/R.java
@@ -14,20 +14,18 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int settings_icon=0x7f020001;
+        public static final int background=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int settings_icon=0x7f020002;
     }
     public static final class id {
-        public static final int AddressEditText=0x7f080001;
-        public static final int ConnectButton=0x7f080005;
-        public static final int DisconnectButton=0x7f080006;
-        public static final int FrameLayout01=0x7f080000;
-        public static final int FrameLayout02=0x7f080004;
-        public static final int ListView01=0x7f080003;
-        public static final int PortEditText=0x7f080002;
-        public static final int about=0x7f080009;
-        public static final int help=0x7f080008;
-        public static final int settings=0x7f080007;
+        public static final int ConnectButton=0x7f080001;
+        public static final int DisconnectButton=0x7f080002;
+        public static final int Status=0x7f080000;
+        public static final int TextView01=0x7f080003;
+        public static final int about=0x7f080006;
+        public static final int help=0x7f080005;
+        public static final int settings=0x7f080004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
